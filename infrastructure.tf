7e6670de7a15feda12edf3a80ceaafb3798bc8b0
iconakin <iconakin@gmail.com>
2020-11-19 02:16:37 -0700

    Added S3 website hosting resource

--- infrastructure.tf
+++ infrastructure.tf
@@ -12,7 +12,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+#Create an S3 bucket with static hosting enabled
+resource "aws_s3_bucket" "tf-web-bucket" {
+  bucket = "hoist-media-blog-site"
+  acl    = "public-read"
+  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+
+  tags = {
+    Name        = "Blog bucket"
+    Environment = "Dev"
+  }
 }
